e119b7707dc9aaae9c02ff5747e6e13c1c297c36
Matt Tanguay-Carel <matt@matthias>
2013-11-26 00:02:10 +0100

    now fetching :popular tweets

--- myapp.rb
+++ myapp.rb
@@ -7,7 +7,7 @@ end
 
 get "/search/:query" do
   client = MyTwitterClient.create_client
-  results = client.search(params[:query], count: 100, exclude: "retweets").map{|r|r}.keep_if{|r| r.retweet_count > 0}
+  results = client.search(params[:query], count: 100, result_type: "popular", exclude: "retweets").map{|r|r}.keep_if{|r| r.retweet_count > 1}
 
   content_type 'application/json', :charset => 'utf-8'
   list = results.map {|t| {username: "@" + t.user.screen_name, user_link: t.user.uri.to_s, retweets: t.retweet_count, tweet: t.text, link: t.uri.to_s}}
